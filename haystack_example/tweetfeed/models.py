d1a00ceb980f05fffac1321fc2f276cb87644944
jgoldberg <jgoldberg@gmail.com>
2011-10-11 17:34:05 -0500

    adding initial models

--- haystack_example/tweetfeed/models.py
+++ haystack_example/tweetfeed/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    favorite_movie = models.CharField(max_length=200)
+
+class Tweet(models.Model):
+    message = models.CharField(max_length=30)
+    created_by = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
