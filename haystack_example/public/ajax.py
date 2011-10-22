3bd0f017ee2d8dce2e058cce319bc1e093a21a85
jgoldberg <jgoldberg@gmail.com>
2011-10-22 00:08:58 -0500

    adding trending web service

--- haystack_example/public/ajax.py
+++ haystack_example/public/ajax.py
@@ -4,6 +4,16 @@ from tweetfeed.models import TweetUser, Tweet
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core import serializers
 from haystack.query import SearchQuerySet
+from haystack.backends.solr_backend import SearchBackend
+from haystack.models import SearchResult
+import settings
+
+try:
+    from pysolr import Solr, SolrError
+except ImportError:
+    raise Exception("The 'solr' backend requires the installation of 'pysolr'. Please refer to the documentation.")
+
+solr_conn = Solr(settings.HAYSTACK_SOLR_URL, timeout=getattr(settings, 'HAYSTACK_SOLR_TIMEOUT', 10))
 
 to_json = lambda data : serializers.serialize("json", data)
 
@@ -20,4 +30,25 @@ def ajax_search(request, query):
 
     tweets = map(lambda _pk : Tweet.objects.get(pk=_pk).to_dict(), [tweet.pk for tweet in results])
 
-    return HttpResponse(json.dumps({'query': query, 'tweets' : tweets }))
+    return HttpResponse(json.dumps({ 'tweets' : tweets }))
+
+def ajax_trending(request):
+    params = {
+        'facet': 'on',
+        'fl': '* score',
+        'start': '0',
+        'facet.field': 'message',
+        'wt': 'json',
+        'fq': 'django_ct:(tweetfeed.tweet)',
+        'facet.prefix': '#',
+        'facet.mincount': '2',
+        'facet.sort': 'count',
+        'facet.limit': '10',
+    }
+    try:
+        result = solr_conn.search('*:*', **params)
+        facets = result.facets['facet_fields']['message']
+    except (IOError, SolrError), e:
+        facets = []
+
+    return HttpResponse(json.dumps({ 'trends' : facets }))
--- haystack_example/public/urls.py
+++ haystack_example/public/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.simple import direct_to_template, redirect_to
 urlpatterns = patterns('public.ajax',
     url(r'ajax/user/(?P<username>.+)$', 'ajax_get_user', name="ajax_get_user"),
     url(r'ajax/search/(?P<query>.*)$', 'ajax_search', name="ajax_search"),
+    url(r'ajax/trending$', 'ajax_trending', name='ajax_trending'),
 )
 
 urlpatterns += patterns('public.views',
